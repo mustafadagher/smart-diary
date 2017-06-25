bf9894994515b38f75fbffa93da2b0acb9f62979
Mustafa Dagher <dagher.mustafa@gmail.com>
2017-06-25 14:44:36 +0200

    update/fix elastic search configs

--- src/main/java/ai/hachathon/smartdiary/SmartDiaryApplication.java
+++ src/main/java/ai/hachathon/smartdiary/SmartDiaryApplication.java
@@ -4,10 +4,12 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
 
@@ -20,11 +22,16 @@ public class SmartDiaryApplication {
 
 	@Bean
 	public Client client() throws UnknownHostException {
-		Settings settings = Settings.settingsBuilder()
+		Settings settings = Settings.builder()
 				.put("cluster.name", "elasticsearch_mustafadagher").build();
-		return TransportClient.builder()
+
+		TransportClient client = new PreBuiltTransportClient(settings)
+                .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+
+        return client;
+        /*return TransportClient.builder()
 				.settings(settings)
 				.build()
-				.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("127.0.0.1", 9200)));
+				.addTransportAddress(new InetSocketTransportAddress(new InetSocketAddress("127.0.0.1", 9300)));*/
 	}
 }
